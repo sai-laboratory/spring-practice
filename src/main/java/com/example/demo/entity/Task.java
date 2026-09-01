55ba0609ec875a818a7230d4000ba883dce65b2d
sai-laboratory <xi1103smh@gmail.com>
2026-09-01 12:00:35 +0900

    Lombok導入

--- src/main/java/com/example/demo/entity/Task.java
+++ src/main/java/com/example/demo/entity/Task.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,14 +18,8 @@ public class Task {
     private String title;
     private int status;
 
-    public Task() {}
-
     public Task(String title, int status) {
         this.title = title;
         this.status = status;
     }
-
-    public Long getId() { return id; }
-    public String getTitle() { return title; }
-    public int getStatus() { return status; }
 }
